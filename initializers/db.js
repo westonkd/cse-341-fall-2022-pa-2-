4fe838974d35a060744c37e91ccb7e79e444bfa5
Weston Dransfield <wdrans@gmail.com>
2022-09-20 20:44:31 -0600

    Correct syntax error

--- initializers/db.js
+++ initializers/db.js
@@ -7,7 +7,7 @@ let db;
 const initialize = async () => {
   if (db) return db;
   try {
-    console.log(JSON.stringify(dbConfig, null, 4)))
+    console.log(JSON.stringify(dbConfig, null, 4));
 
     const client = new MongoClient(dbConfig.connectionString, {
       useNewUrlParser: true,
